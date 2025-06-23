23b8ccfa808a90a97bf92d6944119a33e0b4475f
richaray <rayricha15@gmail.com>
2025-06-23 11:07:07 +0530

    Create Day 10.cpp

---
+++ Day 10.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        // Your code here
+        int sum=0, maxi=INT_MIN;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            
+            if(sum>maxi)
+            maxi=sum;
+            
+            if(sum<0)
+            sum=0;
+        }
+        return maxi;
+    }
+};
