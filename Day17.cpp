006a39d14a217537e2669ca79ee2ba55ff951de2
richaray <rayricha15@gmail.com>
2025-06-24 17:54:45 +0530

    Create Day17.cpp

---
+++ Day17.cpp
@@ -0,0 +1,17 @@
+//Non Repeating character
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        unordered_map<char,int>freq;
+        
+        for(char c:s){
+            freq[c]++;
+        }
+        for(char c : s){
+            if(freq[c]==1)
+            return c;
+        }
+        return '$';
+    }
+};
